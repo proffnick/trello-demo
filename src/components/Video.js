e7a0a2919addfa96d1b2157cdb713cc15ee37572
adimscredit@gmail.com <proffnick1@gmail.com>
2022-05-02 16:45:17 +0100

    make commit

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,12 +2,10 @@ import React from 'react';
 export const Video = () => {
 
     return(
-        
-      <></>  
+      <div className='container col-11 col-lg-9 mx-auto my-4'>
+        <h3 className='text-muted my-3'>How it works</h3>
+      <iframe className='rounded-3 shadow-lg' width="100%" height="515" src="https://www.youtube.com/embed/DEGW9K5JgMI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+      </div>
     );
 }
-
-/*<div className='container col-11 col-lg-9 mx-auto my-4'>
-            <h3 className='text-muted text-center'>How it works!</h3>
-            <iframe className='rounded-3 shadow-lg' width="100%" height="515" src="https://www.youtube.com/embed/0K2jxU7Exco" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>*/
